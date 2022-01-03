88001710c73cce0b65619b347401875e9da01697
Gabrielle <gabinunes09@gmail.com>
2022-01-03 17:38:11 -0300

    first repository

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { ListaDeNotas } from "./components/ListaDeNotas";
+
+function App() {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota: " />
+        <button>Criar nota</button>
+      </form>
+      <ListaDeNotas/>
+    </section>
+  );
+}
+
+export default App;
