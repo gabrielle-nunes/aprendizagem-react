aba23a1a595b849589539daf19fa2bf561ba9028
Gabrielle <gabinunes09@gmail.com>
2022-01-04 07:35:20 -0300

    estilizando o projeto e organizando codigo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { Component } from "react";
-import "./App.css";
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
-import { ListaDeNotas } from "./components/ListaDeNotas";
+import "./assets/App.css";
+import './assets/index.css';
 
 class App extends Component {
-  render (){
+
+  criarNota(titulo, texto){
+    console.log(`uma nova nota foi criada ` + titulo + " " + texto)
+  }
+  render() {
     return (
-      <section>
-        <FormularioCadastro/>
-        <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro criarNota={this.criarNota}/>
+        <ListaDeNotas />
       </section>
     );
   }
-  
 }
 
 export default App;
