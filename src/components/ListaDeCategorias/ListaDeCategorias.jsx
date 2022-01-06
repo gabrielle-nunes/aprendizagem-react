a8ede334ab921e838c567a9829bb32e262b8c070
Gabrielle <gabinunes09@gmail.com>
2022-01-06 07:34:07 -0300

    Adicionando lista de categorias

---
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./ListaDeCategorias.css";
+
+class ListaDeCategorias extends React.Component {
+  render() {
+    return (
+      <section>
+        <ul>
+          <li>Categoria</li>
+          <li>Categoria</li>
+          <li>Categoria</li>
+          <li>Categoria</li>
+        </ul>
+        <input type="text" />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
